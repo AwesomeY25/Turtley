0d31a1f48b44b19e36f9a36535ad78e457dae076
KrimKofi <71881898+KrimKofi@users.noreply.github.com>
2023-04-18 05:28:14 +0800

    added header

--- HackFest - Turtley/hackfest_turtley/lib/insurance_page.dart
+++ HackFest - Turtley/hackfest_turtley/lib/insurance_page.dart
@@ -5,15 +5,35 @@ class InsurancePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-            child: Text("This is our insurance app"
-                // need header
-
-                //search bar
-
-                // panels
-
-                )));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(225, 44, 92, 138),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2 - 27,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(225, 44, 92, 138),
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
