0db5f2cd4dbfe73db4259e51a489b23644514c10
KrimKofi <71881898+KrimKofi@users.noreply.github.com>
2023-04-18 17:13:06 +0800

    adding dchart extension/fixed budget page layout

--- HackFest - Turtley/hackfest_turtley/lib/budget_page.dart
+++ HackFest - Turtley/hackfest_turtley/lib/budget_page.dart
@@ -7,10 +7,10 @@ class BudgetPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
+            child: Stack(
+      children: <Widget>[
+        Container(
             child: Stack(children: <Widget>[
-      Container(
-          child: Stack(
-        children: <Widget>[
           Expanded(
               child: Column(children: [
             Container(
@@ -27,37 +27,61 @@ class BudgetPage extends StatelessWidget {
                     bottomRight: Radius.circular(30),
                     bottomLeft: Radius.circular(30)),
               ),
+              child: ListTile(
+                            title: const Text('Your Income',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold)),
+                            focusColor: Colors.black,
+                          ))),
             ),
             Container(
-                child: Column(children: [
-              Container(
-                height: 150,
-                child: Card(
-                    margin: EdgeInsets.all(15),
-                    child: InkWell(
-                        onTap: () {},
-                        splashColor: Colors.white,
-                        child: Padding(
-                            padding: EdgeInsets.all(13),
-                            child: ListTile(
-                              title: const Text('Level 1',
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                      fontSize: 40,
-                                      fontWeight: FontWeight.bold)),
-                              subtitle: const Text('Financial Institutions',
-                                  style: TextStyle(fontSize: 20)),
-                            ))),
-                    color: Color.fromARGB(225, 44, 92, 138),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      //set border radius more than 50% of height and width to make circle
-                    )),
-              ),
-            ]))
+              height: 150,
+              child: Card(
+                  margin: EdgeInsets.all(15),
+                  child: InkWell(
+                      onTap: () {},
+                      splashColor: Colors.white,
+                      child: Padding(
+                          padding: EdgeInsets.all(13),
+                          child: ListTile(
+                            title: const Text('Your Income',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold)),
+                            focusColor: Colors.black,
+                          ))),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    //set border radius more than 50% of height and width to make circle
+                  )),
+            ),
+            Container(
+              height: 150,
+              child: Card(
+                  margin: EdgeInsets.all(15),
+                  child: InkWell(
+                      onTap: () {},
+                      splashColor: Colors.white,
+                      child: Padding(
+                          padding: EdgeInsets.all(13),
+                          child: ListTile(
+                            title: const Text('Your Income',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold)),
+                            focusColor: Colors.black,
+                          ))),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    //set border radius more than 50% of height and width to make circle
+                  )),
+            ),
           ]))
-        ],
-      ))
-    ])));
+        ]))
+      ],
+    )));
   }
 }
