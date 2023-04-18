3c4a5d12de8f4fb897234c7db67ce51b4c22313e
Yra Corciga <87885042+AwesomeY25@users.noreply.github.com>
2023-04-18 11:04:21 +0800

    reverted changes
    - undo firebase changes

--- HackFest - Turtley/hackfest_turtley/lib/main.dart
+++ HackFest - Turtley/hackfest_turtley/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() => runApp(new MyApp());
 
