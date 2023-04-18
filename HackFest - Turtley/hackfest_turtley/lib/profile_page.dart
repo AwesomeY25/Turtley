2ce01f886c7193883607566dd58c4cd520a58119
Yra Corciga <87885042+AwesomeY25@users.noreply.github.com>
2023-04-19 05:52:38 +0800

    profile page done
    - profile page done

--- HackFest - Turtley/hackfest_turtley/lib/profile_page.dart
+++ HackFest - Turtley/hackfest_turtley/lib/profile_page.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         shrinkWrap: true,
-        children: <Widget>[ SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height * .20,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * .15,
             child: Stack(children: <Widget>[
               Container(
-                height: MediaQuery.of(context).size.height * 0.20 - 40,
+                height: MediaQuery.of(context).size.height * 0.15 - 17,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                       begin: Alignment.topCenter,
@@ -26,71 +25,152 @@ class ProfilePage extends StatelessWidget {
                       bottomRight: Radius.circular(30),
                       bottomLeft: Radius.circular(30)),
                 ),
-              ), 
-              Container(
-                margin: EdgeInsets.all(100.0),
-                decoration: BoxDecoration(
-                color: Colors.orange,
-                shape: BoxShape.circle
-                ),
               ),
-              Padding(
-              padding: const EdgeInsets.only(top: 180, left: 30, right: 30, bottom: 25,),
-              child: Container(
-              alignment: Alignment.center,
-              child: Column(
-              children: [
-                const Text('Jackson Wang', textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                    ],
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child:Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage('https://example.com/my-image.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Row(children: [
+                  Column(children: [
+                    Padding(
+                    padding: const EdgeInsets.only(top: 180, left: 100, bottom: 50,),
+                    child: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                    children: [
+                      const Text('Jackson Wang', textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white,)),
+                  ],
                   ),
                 ),
               ),
-              Row(children: [SingleChildScrollView(
+                ]),
+            ]),) // BoxDecoration
+            ]), // Container
+          ), // Positioned
+         
+          SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
                   Card(
+                      elevation: 20,
                       margin: EdgeInsets.all(15),
                       color: Color.fromARGB(225, 44, 92, 138),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
-
-                        //set border radius more than 50% of height and width to make circle
                       ),
-                      child: SizedBox(
-                        width: 200,
-                        height: 100,
-                      )),
+                      child: Container(
+                          width: 350,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.center,
+                                  colors: [
+                                    Color.fromARGB(224, 96, 142, 169),
+                                    Color.fromARGB(225, 44, 92, 138),
+                                  ]),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: ListTile(
+                            leading: Image.asset(
+                              'assets/images/Egg.png',
+                              height: 700,
+                            ),                         
+                            title: Text("3 days to hatch!", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+                            subtitle: Text("Use the application for 3 days to get your turtle!", style: TextStyle(color: Colors.grey),),
+                          ))),
                   Card(
+                      elevation: 20,
                       margin: EdgeInsets.all(15),
                       color: Color.fromARGB(225, 44, 92, 138),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
-
-                        //set border radius more than 50% of height and width to make circle
                       ),
-                      child: SizedBox(
-                        width: 200,
-                        height: 100,
-                      )),
-                  Card(
-                      margin: EdgeInsets.all(15),
-                      color: Color.fromARGB(225, 44, 92, 138),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-
-                        //set border radius more than 50% of height and width to make circle
-                      ),
-                      child: SizedBox(
-                        width: 200,
-                        height: 100,
-                      ))
+                      child: Container(
+                          width: 350,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.center,
+                                  colors: [
+                                    Color.fromARGB(224, 96, 142, 169),
+                                    Color.fromARGB(225, 44, 92, 138),
+                                  ]),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: ListTile(
+                            leading: Image.asset(
+                              'assets/images/Egg.png',
+                              height: 700,
+                            ),                         
+                            title: Text("3 days to hatch!", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+                            subtitle: Text("Use the application for 3 days to get your turtle!", style: TextStyle(color: Colors.grey),),
+                          ))),
                 ],
               )),
-              ],)
-          
-          ]),)
-                     // BoxDecoration
-                  )]), // Container
-                );}}
+          Column(children: [
+                    Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                    children: [
+                      const Text('Summary', textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.grey,)),
+                  ],
+                  ),
+                ),
+            ]),
+            Row(
+              children: [
+                Center(child: Card(
+                    elevation: 20,
+                    child: Container(
+                      width: 370,
+                      height: 120,
+                      child: ListTile(
+                      title: Text("My Income", style: TextStyle(fontSize: 30),),
+                      subtitle: Text("There is nothing to display here."),
+                    ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    )),)
+              ],
+            ),
+            Row(
+              children: [
+                Center(child: Card(
+                    elevation: 20,
+                    child: Container(
+                      width: 370,
+                      height: 120,
+                      child: ListTile(
+                      title: Text("My Expenses", style: TextStyle(fontSize: 30),),
+                      subtitle: Text("There is nothing to display here."),
+                    ),
+                    ),
+                    margin: EdgeInsets.all(12),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    )),)
+              ],
+            ),
+          ]),
+      );
+  }
+}
